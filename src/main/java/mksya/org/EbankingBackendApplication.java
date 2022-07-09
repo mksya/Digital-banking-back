217ec7cd770d02f3c29cff1a74b0acc15ab04c59
mksya <mik.sam@hotmail.fr>
2022-07-09 17:55:25 +0200

    creation ioc

---
+++ src/main/java/mksya/org/EbankingBackendApplication.java
@@ -0,0 +1,13 @@
+package mksya.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingBackendApplication.class, args);
+	}
+
+}
